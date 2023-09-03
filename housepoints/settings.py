b6f888fe46febff100bf73f762cbc30b605b47a3
Oliver Anthony <oliver.h.anthony@gmail.com>
2023-09-03 13:47:19 +0200

    updated to use postgres

--- housepoints/settings.py
+++ housepoints/settings.py
@@ -104,13 +104,6 @@ WSGI_APPLICATION = 'housepoints.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, "db.sqlite3")
-    }
-}
-
 # !!! UNCOMMENT BELOW FOR HEROKU DEPLOYMENT
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
